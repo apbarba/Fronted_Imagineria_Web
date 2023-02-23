caf4a24a026d91a0869cafdc765604736ba0c744
Ana pilar Barba López <pilarbarba03@gmail.com>
2023-02-23 19:53:16 +0100

    Empezando a implementar otra lista

---
+++ app/lib/pages/categorias_page.dart
@@ -0,0 +1,172 @@
+import 'dart:convert';
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc_authentication/repositories/obras_repository.dart';
+
+import '../models/user.dart';
+import 'addObras_page.dart';
+import 'obras_details.dart';
+import 'package:http/http.dart' as http;
+
+class CategoriasPage extends StatelessWidget {
+  final User user;
+
+  // dynamic _data;
+
+  const CategoriasPage({super.key, required this.user});
+
+  Future<List<dynamic>> fetchData() async {
+    final response =
+        await http.get(Uri.parse('http://localhost:8080/categoria/'), headers: {
+      'Authorization': //modo agitanado
+          'Bearer ${user.token}'
+    });
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body)["content"].toList();
+    } else {
+      throw Exception('Failed to load Categorias');
+    }
+  }
+
+  _navigateAddObras(BuildContext context) async {
+    final result = await Navigator.push(
+      context,
+      MaterialPageRoute(
+          builder: (context) => AddObrasPage(
+                obrasRepository: ObrasRepository(),
+              )),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Categorias'),
+        actions: [
+          ElevatedButton(
+            child: Icon(Icons.add),
+            onPressed: () => _navigateAddObras(context),
+          ),
+        ],
+      ),
+      body: FutureBuilder<List>(
+          future: fetchData(),
+          builder: (context, snapshot) {
+            if (snapshot.hasError) print(snapshot.error);
+
+            return snapshot.hasData
+                ? ItemList(
+                    list: snapshot.requireData,
+                  )
+                : Center(
+                    child: CircularProgressIndicator(color: Colors.purple),
+                  );
+          }),
+    );
+  }
+
+//  @override
+//  Widget build(BuildContext context) {
+//    return Scaffold(
+//      appBar: AppBar(title: Text('My Widget')),
+//      body: FutureBuilder(
+//        future: fetchData(),
+//        builder: (context, snapshot) {
+//          if (snapshot.hasData) {
+//            List<dynamic> data = snapshot.data as List<dynamic>;
+//            return ListView.builder(
+//              itemCount: data.length,
+//              itemBuilder: (context, index) {
+//                return ListTile(
+//                  title: Text(data[index]['name']),
+//                  subtitle: Text(data[index]['name']),
+//                  onTap: () {
+//                    Navigator.push(
+//                      context,
+//                      MaterialPageRoute(
+//                          builder: (context) => ObraDetailsPage(
+//                                obraId: (data[index]['id']),
+//                              )),
+//                    );
+//                  },
+//                );
+//              },
+//            );
+//          } else if (snapshot.hasError) {
+//            return Text("${snapshot.error}");
+//          }
+//          return CircularProgressIndicator();
+//        },
+//      ),
+//    );
+//  }
+}
+
+class ItemList extends StatelessWidget {
+  final List list;
+
+  const ItemList({required this.list});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: list == null ? 0 : list.length,
+      itemBuilder: (context, i) {
+        return Column(
+          children: [
+            Container(
+              padding: EdgeInsets.all(10.0),
+              child: GestureDetector(
+                onTap: () {},
+                child: Container(
+                  height: 100.3,
+                  child: Card(
+                    color: Colors
+                        .primaries[Random().nextInt(Colors.primaries.length)],
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Row(
+                            children: [
+                              Container(
+                                child: Text(
+                                  list[i]['name'].toString(),
+                                  style: TextStyle(
+                                      fontSize: 20.0, color: Colors.black),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Row(
+                            children: [
+                              Container(
+                                child: Text(
+                                  list[i]['titulo'].toString(),
+                                  style: TextStyle(
+                                      fontSize: 15.0, color: Colors.black),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        );
+      },
+    );
+  }
+}
--- app/lib/pages/home_page.dart
+++ app/lib/pages/home_page.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_bloc_authentication/blocs/authentication/authentication.dart';
+import 'package:flutter_bloc_authentication/pages/categorias_page.dart';
 import 'package:flutter_bloc_authentication/pages/obras_page.dart';
 import '../models/models.dart';
 
@@ -15,46 +16,75 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     final authBloc = BlocProvider.of<AuthenticationBloc>(context);
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home Page'),
-      ),
-      body: SafeArea(
-        minimum: const EdgeInsets.all(16),
-        child: Center(
-          child: Column(
-            children: <Widget>[
-              Text(
-                'Welcome, ${user.name}',
-                style: TextStyle(fontSize: 24),
-              ),
-              const SizedBox(
-                height: 12,
-              ),
-              TextButton(
-                //textColor: Theme.of(context).primaryColor,
-                style: TextButton.styleFrom(
-                  primary: Theme.of(context).primaryColor,
-                ),
-                child: Text('Logout'),
-                onPressed: () {
-                  authBloc.add(UserLoggedOut());
-                },
-              ),
-              SizedBox(
-                width: 100,
-                child: TextButton(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => ObrasPage(user: user)));
-                    },
-                    child: Text('Obras')),
-              ),
-            ],
-          ),
+        appBar: AppBar(
+          title: Text('Home Page'),
         ),
-      ),
-    );
+        body: SafeArea(
+          minimum: const EdgeInsets.all(16),
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                Text(
+                  'Welcome, ${user.name}',
+                  style: TextStyle(fontSize: 24),
+                ),
+                const SizedBox(
+                  height: 12,
+                ),
+                TextButton(
+                  //textColor: Theme.of(context).primaryColor,
+                  style: TextButton.styleFrom(
+                    primary: Theme.of(context).primaryColor,
+                  ),
+                  child: Text('Logout'),
+                  onPressed: () {
+                    authBloc.add(UserLoggedOut());
+                  },
+                ),
+                SizedBox(
+                  width: 100,
+                  child: TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => ObrasPage(user: user)));
+                      },
+                      child: Text('Obras')),
+                ),
+                SizedBox(
+                  width: 100,
+                  child: TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) =>
+                                    CategoriasPage(user: user)));
+                      },
+                      child: Text('Categorias')),
+                ),
+              ],
+            ),
+          ),
+        )
+        /*   bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.construction),
+            label: 'Obras',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.engineering),
+            label: 'maginero',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category),
+            label: 'Categorias',
+          ),
+        ],
+      ),*/
+        );
   }
 }
